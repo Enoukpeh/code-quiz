04d2b719dfee5438657c0a700aebad1047fc3f18
Enoobong <enoukpeh@yahoo.com>
2021-07-04 17:26:40 -0400

    changes to jscript and html

---
+++ script.js
@@ -0,0 +1,17 @@
+function buildQuiz(){}
+// variable to store html output
+const output = [];
+// for wach questions
+myQuestion.forEach(
+(currentQuestion, uuestionNumber) => {
+    
+}
+
+
+function showResults(){}
+
+// display quiz right away
+buildQuiz();
+
+// on submit, show results
+submitButton.addEventListener('click', showResults);
